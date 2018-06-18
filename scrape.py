1e960a72a0534eb5fd713b5d2e555b975eff3136
Ali <atajadod94@gmail.com>
2018-06-18 11:14:54 -0400

    added multithread
    processing to the scraper

--- scrape.py
+++ scrape.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python2.7
 
-from joblib import Parallel, delayed
-import multiprocessing
+from multiprocessing import Pool
 import urllib2
 import re
 import sys
@@ -13,6 +12,8 @@ import matplotlib.pyplot as plt
 import requests
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import os
+from time import sleep
+
 
 
 
@@ -23,7 +24,7 @@ def download(url, user_agent='wswp', num_retries=2):
     try:
         html = urllib2.urlopen(request).read()
     except urllib2.URLError as e:
-        print 'Download error:', e.reason
+        print ('Download error:', e.reason)
         html = None
         if num_retries > 0:
             if hasattr(e, 'code') and 500 <= e.code < 600:
@@ -90,24 +91,22 @@ def blogplot(user='ak73'):
     blogs_text = re.findall('<span class="info">(.*?)</span>', html)
     numblogs = filter(str.isdigit, blogs_text.__str__())
     soup = BeautifulSoup(html, 'html.parser')
-    num_pages = (int(numblogs) / 6) + 1
+    num_pages = (int(numblogs) / 6) +1
     page_blogs = soup.find(attrs={'class': 'blogPosts'})
     comments_db = pd.DataFrame(columns=['user', 'num_comments'])
     top_blog = 0
-
     blogs = page_blogs.findAll('a')
 
     for blog in blogs:
         if blog.has_attr('rel') and blog.attrs['rel'] and blog.attrs['rel'][0].startswith('bookmark'):
             blog_link = 'https://tengaged.com' + blog.attrs['href'].__str__()
-            blog_html = download(blog_link)
-            soup = BeautifulSoup(blog_html)
+            blog_html = requests.get(blog_link).text
+            soup = BeautifulSoup(blog_html,"lxml")
 
             if re.findall('<span class="floated miniadrank">(.*?)</span>', blog_html):
                 top_blog += 1
 
             comments = soup.findAll(attrs={'blogPostComments'})
-
             for comment in comments:
                 tail = comment.find(attrs={'class': 'tail'})
                 cuser = tail.find('a').text.__str__()
@@ -118,51 +117,69 @@ def blogplot(user='ak73'):
                 else:
                     comments_db = comments_db.append({'user': cuser, 'num_comments': 1}, ignore_index=True)
 
-    def processBlogs(i):
-        print(i)
-        link = 'https://tengaged.com/blog/' + user + '/page/' + str(i)
-        html = download(link)
-        soup = BeautifulSoup(html, "lxml")
-
+    blog_links = ('https://tengaged.com/blog/' + user + '/page/' + str(i) for i in range(1, num_pages + 1))
+    ## first multi process
+    p = Pool(processes=10)
+    page_html = p.map(gethtml, blog_links)  ## passing blog_links as a generator
+    p.close()
+    blog_links = list()
+    for html in page_html:
+        soup = BeautifulSoup(html.text, "lxml")
         page_blogs = soup.find(attrs={'class': 'blogPosts'})
         blogs = page_blogs.findAll('a')
-
         for blog in blogs:
             if blog.has_attr('rel') and blog.attrs['rel'] and blog.attrs['rel'][0].startswith('bookmark'):
-                blog_link = 'https://tengaged.com' + blog.attrs['href'].__str__()
-                blog_html = download(blog_link)
-                soup = BeautifulSoup(blog_html, "lxml")
+                blog_links += ['https://tengaged.com' + blog.attrs['href'].__str__()]
 
-                if re.findall('<span class="floated miniadrank">(.*?)</span>', blog_html):
-                    top_blog += 1
+    p = Pool(processes=10)
+    blot_html_all = p.map(gethtml, blog_links)# passing blog_links as a generator
+    p.close()
 
-                comments = soup.findAll(attrs={'blogPostComments'})
+    top_blog = 0
+    comments = []
+    for blog_html in blot_html_all:
+        soup = BeautifulSoup(blog_html.text, "lxml")
+        if re.findall('<span class="floated miniadrank">(.*?)</span>', blog_html.text):
+            top_blog += 1
 
-                for comment in comments:
-                    tail = comment.find(attrs={'class': 'tail'})
-                    cuser = tail.find('a').text.__str__()
-                    if cuser in list(comments_db.user):
-                        index = comments_db.user[(comments_db.user == cuser)].index[0]
-                        current_count = comments_db.iloc[index].num_comments
-                        comments_db.set_value(index, 'num_comments', current_count + 1)
-                    else:
-                        comments_db = comments_db.append({'user': cuser, 'num_comments': 1}, ignore_index=True)
+        comments += [soup.findAll(attrs={'blogPostComments'})]
 
-    inputs = range(1,2)# range(1, min(num_pages + 1, 100))
-    num_cores = multiprocessing.cpu_count()
-    Parallel(n_jobs=num_cores)(delayed(processBlogs)(i) for i in inputs)
+    all_comments = (_ for blog_comment in comments for _ in blog_comment)
 
+    for comment in all_comments:
+        tail = comment.find(attrs={'class': 'tail'})
+        cuser = tail.find('a').text.__str__()
+        if cuser in list(comments_db.user):
+            index = comments_db.user[(comments_db.user == cuser)].index[0]
+            current_count = comments_db.iloc[index].num_comments
+            comments_db.set_value(index, 'num_comments', current_count + 1)
+        else:
+            comments_db = comments_db.append({'user': cuser, 'num_comments': 1}, ignore_index=True)
 
     comments_db.num_comments = comments_db.num_comments.astype(int)
-    top10 = (comments_db.nlargest(20, 'num_comments'))
-    top_blog_percentage = 100 * (float(top_blog) / (i * 6))
+    top10 = (comments_db.nlargest(5, 'num_comments'))
+    top_blog_percentage = 100 * (float(top_blog) / (num_pages * 6))
 
 
-    a = sns.barplot(top10.user, top10.num_comments).set_title(
-        user + '   top blog percentage:    ' + str(top_blog_percentage))
+    a = sns.barplot(top10.user, top10.num_comments)
+    a.set_title(user + '   top blog percentage:    ' + str(top_blog_percentage))
     b = a.axes
     b.set_xticklabels(b.get_xticklabels(), rotation=40, ha="right")
     plt.savefig('blog_data/' + user)
+
+def gethtml(url, user_agent='tblade' , num_retries = 5):
+    headers = {'User-agent': user_agent}
+    url_html = requests.get(url)
+    if url_html.ok:
+        return url_html
+    elif url_html.error:
+        print('Download error:', url_html.error.reason)
+        if num_retries > 0:
+            if hasattr(url_html.error, 'code') and 500 <= url_html.error.code < 600:
+                # retry 5XX HTTP errors
+                return gethtml(url, user_agent, num_retries - 1)
+
+
 
 
 def giftplot(user='ak73'):
@@ -195,6 +212,7 @@ def giftplot(user='ak73'):
     b = a.axes
     b.set_xticklabels(b.get_xticklabels(), rotation=40, ha="right")
     plt.savefig('gift_data/' + user)
+
 
 #blogplot('joe1110')
 #gameplot('joe1110')
